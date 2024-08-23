56c93108f106e04874dce773921ea6ac1ee22544
HoyeonS <100548830+HoyeonS@users.noreply.github.com>
2024-08-22 22:06:40 -0700

    initalize the project

---
+++ src/main/java/com/example/codehive_auth/CodehiveAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.codehive_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodehiveAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodehiveAuthApplication.class, args);
+	}
+
+}
